994ef73ac30915512abae9400371045dd9e0f7a8
Megha.Naik <meghnanaik713@gmail.com>
2020-10-23 15:39:15 +0530

    Added test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 def add(num1, num2):
     return num1+num2
 
+
 if __name__ == '__main__':
-    num1 = int(input("Enter Number1"))
-    num2 = int(input("Enter Number2"))
-    print(add(num1, num2))
+    number1 = int(input("Enter Number1"))
+    number2 = int(input("Enter Number2"))
+    print(add(number1, number2))
